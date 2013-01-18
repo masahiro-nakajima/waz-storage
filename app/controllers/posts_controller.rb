0258c7946ae0fc58b7b437cebb9bb5422af2f912
Daichi Arai <daichi2k@gmail.com>
2013-01-18 17:31:26 +0900

    add posts controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,46 @@
+class PostsController < ApplicationController
+
+  #
+  # GET /posts
+  #
+  def index
+  end
+
+  #
+  # GET /posts/:id
+  #
+  def show
+  end
+
+  #
+  # GET /posts/new
+  #
+  def new
+  end
+
+  #
+  # POST /posts
+  #
+  def create
+  end
+
+  #
+  # GET /posts/:id/edit
+  #
+  def edit
+  end
+
+
+  #
+  # PUT /posts/:id
+  #
+  def update
+  end
+
+  #
+  # DELETE /posts/:id
+  #
+  def destroy
+  end
+
+end
